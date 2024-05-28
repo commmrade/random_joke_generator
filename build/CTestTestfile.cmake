8f95d3f191c98c58c226cdc79b0178b9bb6056b8
klewyyy <45066731+commmrade@users.noreply.github.com>
2024-05-28 17:03:57 +1000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/klewy/programming/vscode/cpp_projects/randomjoke
-# Build directory: /home/klewy/programming/vscode/cpp_projects/randomjoke/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
